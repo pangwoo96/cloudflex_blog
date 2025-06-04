3a19c7b058dc55273ba82e034f9497b96eeab125
pangwoo96 <junggw96@gmail.com>
2025-06-04 23:40:47 +0900

    chore: git init

---
+++ src/main/java/com/cloudflex_blog/CloudflexBlogApplication.java
@@ -0,0 +1,13 @@
+package com.cloudflex_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflexBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudflexBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloudflex_blog/CloudflexBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudflex_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudflexBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
